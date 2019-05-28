1b5c232a89279f32e82c8774046d4d9f4cbcd90b
Feng Guo <feng.guo2001@gmail.com>
2019-05-28 16:36:18 -0400

    I'll update this later

--- src/World/Location.java
+++ src/World/Location.java
@@ -7,22 +7,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 abstract public class Location {
-    Image background;
-    ArrayList<Double> playerIDs;
-    ArrayList<Double> npcIDs;
-    ArrayList<Double> enemyIDs;
-    ArrayList<Double> weaponEffectIDs;
-    ArrayList<Double> environmentalIDs;
-    ArrayList<Double> itemDropIDs;
-    HashMap<Double, PlayerHandler> playerHandlerHashMap;
-    HashMap<Double, NPCHandler> npcHandlerHashMap;
-    HashMap<Double, EnemyHandler> enemyHandlerHashMap;
-    HashMap<Double, WeaponEffectHandler> weaponEffectHandlerHashMap;
-    HashMap<Double, EnvironmentalInstance> environmentalInstanceHashMap;
-    HashMap<Double, ItemDropInstance> itemDropInstanceHashMap;
+    private Image background;
+    private ArrayList<Double> playerIDs;
+    private ArrayList<Double> npcIDs;
+    private ArrayList<Double> enemyIDs;
+    private ArrayList<Double> weaponEffectIDs;
+    private ArrayList<Double> environmentalIDs;
+    private ArrayList<Double> itemDropIDs;
+    private HashMap<Double, PlayerHandler> playerHandlerHashMap;
+    private HashMap<Double, NPCHandler> npcHandlerHashMap;
+    private HashMap<Double, EnemyHandler> enemyHandlerHashMap;
+    private HashMap<Double, WeaponEffectHandler> weaponEffectHandlerHashMap;
+    private HashMap<Double, EnvironmentalInstance> environmentalInstanceHashMap;
+    private HashMap<Double, ItemDropInstance> itemDropInstanceHashMap;
 
     public Location(Image background) {
         this.background = background;
+        playerIDs = new ArrayList<>();
+        npcIDs = new ArrayList<>();
+        enemyIDs = new ArrayList<>();
+        weaponEffectIDs = new ArrayList<>();
+        environmentalIDs = new ArrayList<>();
+        itemDropIDs = new ArrayList<>();
         playerHandlerHashMap = new HashMap<>();
         npcHandlerHashMap = new HashMap<>();
         enemyHandlerHashMap = new HashMap<>();
@@ -30,6 +36,4 @@ abstract public class Location {
         environmentalInstanceHashMap = new HashMap<>();
         itemDropInstanceHashMap = new HashMap<>();
     }
-
-
 }
