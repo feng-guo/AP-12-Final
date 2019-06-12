c7a0748a6a123a3a071af6f5d86e24d325b5afcc
Feng Guo <feng.guo2001@gmail.com>
2019-06-12 19:23:42 -0400

    you can now attack and it draws the attack sprite

--- src/World/LocationHandler.java
+++ src/World/LocationHandler.java
@@ -6,7 +6,9 @@ import Items.Stack;
 import Items.Tool;
 import Items.Weapon;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class LocationHandler implements Runnable {
     private Location location;
@@ -274,11 +276,13 @@ public class LocationHandler implements Runnable {
         double max = (-1/Math.pow(playerHandler.getPlayerInstance().getDexterity()/10, 2)+1) +1.75;
         if (delta > 2.75 - max) {
             Weapon weapon = (Weapon) playerHandler.getPlayerInstance().getInventory().getCurrentItem().getItem();
-            WeaponEffect weaponEffect = new WeaponEffect(null, 2, 2, weapon); //THIS IS NOT REAL CODE
-            WeaponEffectInstance weaponEffectInstance = new WeaponEffectInstance(playerHandler.getPlayerInstance().getX(), playerHandler.getPlayerInstance().getY(), weaponEffect, System.nanoTime()/1e+9, x, y, (double)System.nanoTime());
+            int newX = playerHandler.getPlayerInstance().getX() + playerHandler.getPlayerInstance().getEntity().getLength()/2;
+            int newY = playerHandler.getPlayerInstance().getY() + playerHandler.getPlayerInstance().getEntity().getWidth()/2;
+            WeaponEffect weaponEffect = new WeaponEffect(Toolkit.getDefaultToolkit().createImage("Bread.png"), 30, 30, weapon);
+            WeaponEffectInstance weaponEffectInstance = new WeaponEffectInstance(newX, newY, weaponEffect, System.nanoTime()/1e+9, x, y, (double)System.nanoTime());
             WeaponEffectHandler weaponEffectHandler = new WeaponEffectHandler(weaponEffectInstance, this);
-            weaponEffectIDs.add(weaponEffectInstance.getID());
-            weaponEffectHandlerHashMap.put(weaponEffectInstance.getID(),weaponEffectHandler);
+            addWeaponEffect(weaponEffectHandler);
+
             playerHandler.setLastWeaponUse(currentTime);
             
             /*
@@ -300,7 +304,7 @@ public class LocationHandler implements Runnable {
         double max = (-1/Math.pow(enemyHandler.getEnemyInstance().getDexterity()/10, 2)+1) +1.75;
         if (delta > 2.75 - max) {
             Weapon weapon = enemyHandler.getEnemyInstance().getWeapon();
-            WeaponEffect weaponEffect = new WeaponEffect(null, 2, 2, weapon); //THIS IS NOT REAL CODE
+            WeaponEffect weaponEffect = new WeaponEffect(null, 20, 20, weapon); //THIS IS NOT REAL CODE
             WeaponEffectInstance weaponEffectInstance = new WeaponEffectInstance(enemyHandler.getEnemyInstance().getX(), enemyHandler.getEnemyInstance().getY(), weaponEffect, System.nanoTime()/1e+9, x, y, (double)System.nanoTime());
             WeaponEffectHandler weaponEffectHandler = new WeaponEffectHandler(weaponEffectInstance, this);
             weaponEffectIDs.add(weaponEffectInstance.getID());
